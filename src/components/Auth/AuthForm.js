89fa3d01d87839ff96e7096ef5eab8ba53b14220
shivam5676 <ssultanpura9@gmail.com>
2023-05-31 10:22:24 -0700

    password change handler attached with firebase,now user can change password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -2,13 +2,14 @@ import { useState, useRef, useContext } from "react";
 
 import classes from "./AuthForm.module.css";
 import HeaderContext from "../../store/HeaderContext";
+import ProfileForm from "../Profile/ProfileForm";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [loading, setIsLoading] = useState(true);
   const emailAuthRef = useRef();
   const passwordAuthRef = useRef();
- const ctx= useContext(HeaderContext)
+  const ctx = useContext(HeaderContext);
   const submitHandler = (event) => {
     event.preventDefault();
     setIsLoading(false);
@@ -27,22 +28,22 @@ const AuthForm = () => {
             "Content-Type": "application/json",
           },
         }
-      ) .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Authentication failed.");
-        }
-      })
-      .then((data) => {
-        
-        ctx.addToken(data.idToken)
-        
-        // Store the token in context
-      })
-      .catch((err) => {
-        alert(err)
-      });
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error("Authentication failed.");
+          }
+        })
+        .then((data) => {
+          ctx.addToken(data.idToken);
+
+          // Store the token in context
+        })
+        .catch((err) => {
+          alert(err);
+        });
     } else {
       fetch(
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDbQjM5ZHO-YFs3fe1rVxJb0vKfGBYqgWs",
@@ -58,19 +59,19 @@ const AuthForm = () => {
           },
         }
       )
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Authentication failed.");
-        }
-      })
-      .then((data) => {
-        console.log(data.idToken); // Store the token in state
-      })
-      .catch((err) => {
-        alert(err)
-      });
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error("Authentication failed.");
+          }
+        })
+        .then((data) => {
+          console.log(data.idToken); // Store the token in state
+        })
+        .catch((err) => {
+          alert(err);
+        });
     }
   };
 
@@ -79,33 +80,44 @@ const AuthForm = () => {
   };
 
   return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required ref={emailAuthRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required ref={passwordAuthRef} />
-        </div>
-        <div className={classes.actions}>
-          {!loading ? (
-            <p>sending request.....</p>
-          ) : (
-            <button>{isLogin ? "Login" : "Create  account"}</button>
-          )}
-          <button
-            type="button"
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? "Create new account" : "Login with existing account"}
-          </button>
-        </div>
-      </form>
-    </section>
+    <div>
+      {!ctx.token ? (
+        <section className={classes.auth}>
+          <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+          <form onSubmit={submitHandler}>
+            <div className={classes.control}>
+              <label htmlFor="email">Your Email</label>
+              <input type="email" id="email" required ref={emailAuthRef} />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Your Password</label>
+              <input
+                type="password"
+                id="password"
+                required
+                ref={passwordAuthRef}
+              />
+            </div>
+            <div className={classes.actions}>
+              {!loading ? (
+                <p>sending request.....</p>
+              ) : (
+                <button>{isLogin ? "Login" : "Create  account"}</button>
+              )}
+              <button
+                type="button"
+                className={classes.toggle}
+                onClick={switchAuthModeHandler}
+              >
+                {isLogin ? "Create new account" : "Login with existing account"}
+              </button>
+            </div>
+          </form>
+        </section>
+      ) : (
+        <ProfileForm></ProfileForm>
+      )}
+    </div>
   );
 };
 
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import HeaderContext from "../../store/HeaderContext";
 
 const ProfileForm = () => {
+  const ctx = useContext(HeaderContext);
+
+  const passwordRef = useRef();
+  const passwordChangeHandler = (event) => {
+    event.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDbQjM5ZHO-YFs3fe1rVxJb0vKfGBYqgWs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: ctx.token,
+          password: passwordRef.current.value,
+          returnSecureToken: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("update failed");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={passwordChangeHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={passwordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
